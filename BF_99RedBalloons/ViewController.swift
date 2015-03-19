418489cfeca659eac8bb5d9e05b4c21cbf034a1a
bdwalker1 <bdwalker@ureach.com>
2015-03-19 02:15:53 -0700

    Added ability to go to specific balloon

--- BF_99RedBalloons/ViewController.swift
+++ BF_99RedBalloons/ViewController.swift
@@ -8,6 +8,18 @@
 
 import UIKit
 
+extension String {
+    func isInteger() -> Bool {
+        enum F {
+            static let formatter = NSNumberFormatter()
+        }
+        if let number = F.formatter.numberFromString(self) {
+            return true
+        }
+        return false
+    }
+}
+
 class ViewController: UIViewController {
 
     @IBOutlet weak var imgBalloon: UIImageView!
@@ -15,6 +27,7 @@ class ViewController: UIViewController {
     
     var balloons:[Balloon] = []
     var nCurrentBalloon:Int = -1
+    var txtBalloon:UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -28,6 +41,46 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func pressGoto(sender: UIBarButtonItem) {
+        
+        var alertGoTo:UIAlertController = UIAlertController(title: "Go To Balloon", message: "Enter balloon number", preferredStyle: UIAlertControllerStyle.Alert)
+        alertGoTo.addTextFieldWithConfigurationHandler(self.setupGoToTextField)
+        alertGoTo.addAction(UIAlertAction(title: "Go", style: UIAlertActionStyle.Default, handler:{ (UIAlertAction)in
+            println("User click Go button")
+            println(self.txtBalloon.text)
+            
+            if (!self.txtBalloon.text.isEmpty)
+            {
+                var strBalloon = self.txtBalloon.text
+                if ( strBalloon.isInteger() )
+                {
+                    var nBalloon = Int(strBalloon.toInt()!)
+                    
+                    if ( nBalloon >= 1 && nBalloon <= 99 )
+                    {
+                        self.nCurrentBalloon = nBalloon - 1
+                        
+                        UIView.transitionWithView(self.view, duration: 0.3, options: UIViewAnimationOptions.TransitionCrossDissolve, animations:  {
+                            self.updateBalloon(self.nCurrentBalloon)
+                            }, completion: { (finished: Bool) -> () in } )
+                    }
+                }
+            }
+        }))
+        self.presentViewController(alertGoTo, animated: true, completion: {
+            println("completion block")
+        })
+    }
+    
+    func setupGoToTextField( txtBalloon: UITextField!){
+        if let tField = txtBalloon {
+            self.txtBalloon = txtBalloon
+            txtBalloon.placeholder = "Enter Balloon Number"
+            txtBalloon.keyboardType = UIKeyboardType.NumberPad
+        }
+
+    }
+    
     @IBAction func pressNext(sender: UIBarButtonItem) {
 //        var nBalloon:Int
 //        do
